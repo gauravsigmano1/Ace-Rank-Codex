2dfbf42fdfb38a71196b2428881a939b2d2f83b7
gauravsigmano1 <153618577+gauravsigmano1@users.noreply.github.com>
2026-07-20 18:19:01 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ace-rank-v5';
+const CACHE = 'ace-rank-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './ace-rank-icon.svg'];
 
 self.addEventListener('install', event => {
