a2f6bf8a973e729ccf247172d500dfab4afe8f5b
gauravsigmano1 <153618577+gauravsigmano1@users.noreply.github.com>
2026-08-07 14:35:24 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'rallysh-v8';
+const CACHE = 'rallysh-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './ace-rank-icon.svg', './rallysh-opening-preview.jpg'];
 
 self.addEventListener('install', event => {
