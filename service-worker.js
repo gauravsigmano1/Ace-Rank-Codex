126ce3b9d85c1c4e7dfc190d963fa2171a9d34e5
gauravsigmano1 <153618577+gauravsigmano1@users.noreply.github.com>
2026-09-16 18:30:36 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'rallysh-v28';
+const CACHE = 'rallysh-v29';
 const IMAGE_CACHE = 'rallysh-image-cache-v1';
 const ASSETS = ['./', './index.html', './safety.js', './terms.html', './community-guidelines.html', './support.html', './manifest.webmanifest', './ace-rank-icon.svg', './rallysh-opening-preview.jpg'];
 
